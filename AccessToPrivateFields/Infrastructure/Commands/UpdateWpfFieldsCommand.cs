ba0cde7c7753a7007ecb4bbd71ff5535ac9ba297
Andrew Muhlynin <rx.081@hotmail.com>
2024-06-17 23:16:00 +0500

    реализована загрузка методов FiledInfo и NestedClass

---
+++ AccessToPrivateFields/Infrastructure/Commands/UpdateWpfFieldsCommand.cs
@@ -0,0 +1,24 @@
+﻿using AccessToPrivateFields.ViewModels;
+using AccessToPrivateFields.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessToPrivateFields.Infrastructure.Commands
+{
+    internal class UpdateWpfFieldsCommand : Base.CommandBase
+    {
+        public override bool CanExecute(object parameter) => true;
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public override void Execute(object parameter)
+        {
+            var mainWindowViewModel = ((parameter as MainWindow).DataContext as MainWindowViewModel);
+            mainWindowViewModel.UpdateMethod();
+        }
+    }
+}
--- AccessToPrivateFields/ViewModels/MainWindowViewModel.cs
+++ AccessToPrivateFields/ViewModels/MainWindowViewModel.cs
@@ -9,20 +9,30 @@ namespace AccessToPrivateFields.ViewModels
 {
     internal class MainWindowViewModel : ViewModels.Base.ViewModelBase
     {
-        public MainWindowViewModel() { OnStartup(); }
-        private void OnStartup()
+        public MainWindowViewModel() {/* UpdateMethod(); */}
+        public void UpdateMethod()
         {
             FieldInfoMethod();
+            NestedClassAccessMethod();
         }
         #region поля для WPF
         #region Ex 1
-        private string _example1TimeSpan;
-        public string Example1TimeSpan { get { return _example1TimeSpan; } set { _example1TimeSpan = value; OnPropertyChanged("TimeSpan1"); } }
+        private string _example1TimeSpan = "TimeSpan1";
+        public string Example1TimeSpan { get => _example1TimeSpan; set => Set(ref _example1TimeSpan, value); } 
         private string _example1Result = "sample1";
-        public string Example1Result { get { return _example1Result; } set { _example1Result = value; OnPropertyChanged("Result1"); } }
-        #endregion        
-        
+        public string Example1Result { get => _example1Result; set => Set(ref _example1Result, value); }
         #endregion
+
+        #region Ex 2
+        private string _example2TimeSpan = "TimeSpan2";
+        public string Example2TimeSpan { get => _example2TimeSpan; set => Set(ref _example2TimeSpan, value); }
+        private string _example2Result = "sample2";
+        public string Example2Result { get => _example2Result; set => Set(ref _example2Result, value); }
+        #endregion
+
+        #endregion
+
+
         #region Example 1: Field Info
 
         private void FieldInfoMethod()
@@ -39,10 +49,27 @@ namespace AccessToPrivateFields.ViewModels
             {
                 result = (string)fieldInfo.GetValue(_projectModel);
             }
+            Example1Result = result;
             var EndTime = DateTime.Now;
             var timeSpan = (EndTime - startTime).TotalMilliseconds;
-            Example1Result = result;
+            
             Example1TimeSpan = timeSpan.ToString() + " ms";
+        }
+        #endregion
+
+        #region Example 2: Nested Class
+
+        private void NestedClassAccessMethod()
+        {
+            var startTime = DateTime.Now;
+            Models.ProjectModel _projectModel = new Models.ProjectModel();
+            Models.ProjectModel.NestedClassToGetAccessToPrivateField _nestedClass = new ProjectModel.NestedClassToGetAccessToPrivateField(_projectModel);
+            Example2Result = _nestedClass.GetPrivateFieldData();
+            var endTime = DateTime.Now;
+            Example2TimeSpan = (endTime - startTime)
+                .Milliseconds
+                .ToString() + " ms";
+            
         }
         #endregion
     }
